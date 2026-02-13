0dfce72a2e9dc07886c1d99110b2ed651da407e2
HIUNDERSCORE <128417733+HIUNDERSCORE@users.noreply.github.com>
2026-02-12 18:14:30 -0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,13 +2,16 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { uvPath } from '@titaniumnetwork-dev/ultraviolet';
 import { createBareServer } from '@tomphttp/bare-server-node';
+import path from 'node:path';
 
 const app = express();
 const bare = createBareServer('/bare/');
 const server = createServer();
+const __dirname = path.resolve();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uv/', express.static(uvPath));
+
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
 server.on('request', (req, res) => {
@@ -27,4 +30,7 @@ server.on('upgrade', (req, socket, head) => {
     }
 });
 
-server.listen(process.env.PORT || 8080, '0.0.0.0');
+const port = process.env.PORT || 10000;
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server live on port ${port}`);
+});
